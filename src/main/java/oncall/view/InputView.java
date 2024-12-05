d62b3fd7f1933855144d50aa1d47104600cb2462
이창헌 <changbill@naver.com>
2024-12-05 12:53:10 +0900

    feat(inputView): add input feature

---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,27 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputAssignMonthAndStartDayOfTheWeek() {
+        System.out.print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+        String result = Console.readLine();
+        System.out.println();
+        return result;
+    }
+
+    public String inputWeekdaysOnCallPlace() {
+        System.out.print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        String result = Console.readLine();
+        System.out.println();
+        return result;
+    }
+
+    public String inputWeekendsOnCallPlace() {
+        System.out.print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        String result = Console.readLine();
+        System.out.println();
+        return result;
+    }
+}
